02aff1b79bc3ab78b57961a1f84dd0fe027f6696
Ilya Glazyrin <glazyrinilya2011@gmail.com>
2021-02-28 01:39:28 +0500

    added homework

--- task.js
+++ task.js
@@ -1,14 +1,46 @@
+"use strict";
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    
+    let x = [];
+    let D = b ** 2 - 4 * a * c;
+    
+    if(D < 0) {
+        return x;
+    } else if (D === 0) {
+        x.push ((-b + Math.sqrt(D)) / (2 * a));
+    } else if (D > 0) {
+        x.push((-b + Math.sqrt(D)) / (2 * a));
+        x.push((-b - Math.sqrt(D)) / (2 * a));
+    }
+    
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+function getAverageMark(marks) {
+
+    let totalSum = 0;
+    marks.splice(5);
+
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        totalSum += marks[i];
+    }
+
+    return totalSum/marks.length;;
+ }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+
+    let age = dateOfBirthday.getFullYear();
+    let date = new Date().getFullYear();
+
+    if ((date - age) >= 18) {
+        return (`Не желаете ли олд-фэшн ${name}?`);
+    } else {
+        return (`Сожалею, ${name},но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
+    }
 }
